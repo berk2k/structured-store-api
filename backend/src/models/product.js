10af0dea4106048fcd0230dc73014f9081950b28
Berk Polat <berkpolat125@gmail.com>
2025-10-07 11:50:45 +0300

    model extend

--- backend/src/models/product.js
+++ backend/src/models/product.js
@@ -2,10 +2,43 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../config/database");
 
 const Product = sequelize.define("Product", {
-  name: DataTypes.STRING,
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
   description: DataTypes.STRING,
-  price: DataTypes.FLOAT,
-  stock: DataTypes.INTEGER,
+  price: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  stock: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  },
+  image: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  category: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  brand: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  rating: {
+    type: DataTypes.FLOAT,
+    defaultValue: 4.5,
+  },
+  reviewCount: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  },
+  currency: {
+    type: DataTypes.STRING,
+    defaultValue: "USD",
+  },
 });
 
 module.exports = Product;
